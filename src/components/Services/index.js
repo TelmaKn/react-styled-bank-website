982e4aa66d778064a433972ce9f98ba224de341c
Telma <telmakn@gmail.com>
2021-09-01 17:29:03 -0300

    first commit

---
+++ src/components/Services/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Icon1 from '../../images/svg-1.svg'
+import Icon2 from '../../images/svg-3.svg'
+import Icon3 from '../../images/svg-4.svg'
+import {
+  ServicesContainer,
+  ServicesH1,
+  ServicesWrapper,
+  ServicesCard,
+  ServicesIcon,
+  ServicesH2,
+  ServicesP,
+} from './ServiceElements'
+
+const Services = () => {
+  return (
+    <ServicesContainer id='services'>
+      <ServicesH1>
+        <ServicesWrapper>
+          <ServicesCard>
+            <ServicesIcon src={Icon1} />
+            <ServicesH2>Reduce expenses</ServicesH2>
+            <ServicesP>
+              We help reduce your fess and increase your overall revenue
+            </ServicesP>
+          </ServicesCard>
+
+          <ServicesCard>
+            <ServicesIcon src={Icon2} />
+            <ServicesH2>Virtual Offices</ServicesH2>
+            <ServicesP>
+              You can acces our platfform online anywhere in the world
+            </ServicesP>
+          </ServicesCard>
+
+          <ServicesCard>
+            <ServicesIcon src={Icon3} />
+            <ServicesH2>Premium Benefit</ServicesH2>
+            <ServicesP>
+              Unlock our special membership card that return 5% cash back.
+            </ServicesP>
+          </ServicesCard>
+        </ServicesWrapper>
+      </ServicesH1>
+    </ServicesContainer>
+  )
+}
+
+export default Services
